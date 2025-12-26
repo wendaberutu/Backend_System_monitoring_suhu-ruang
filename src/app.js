d4d6cd65ee8a15af1514c927cab119de0ec8cf75
wenda <wendaberutu22@gmail.com>
2025-12-26 08:45:43 +0700

    memperbaiki cors supaya bisa di akses siapa pun

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/monitoring", monitoringRoutes);
--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
+// require("dotenv").config();
+// const app = require("./app");
+
+// const PORT = process.env.PORT || 4000;
+// app.listen(PORT, () => {
+//   console.log(`API running on http://localhost:${PORT}`);
+// });
+
+
 require("dotenv").config();
 const app = require("./app");
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`API running on http://localhost:${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`API running on port ${PORT}`);
 });
